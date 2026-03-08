36c3eb06dc96886bf308c4877e19ae37020361df
Divyanshu Anand <79203476+diyanshu-anand@users.noreply.github.com>
2026-03-08 16:36:20 +0530

    Update dashboard.py

--- Frontend/dashboard.py
+++ Frontend/dashboard.py
@@ -1352,10 +1352,10 @@ while True:
             except: pass
             
             # IR FLAME ALERT (0 = danger)
-            try:
-                if "ir_detect" in latest and int(latest["ir_detect"]) == 0:
-                    alert_messages.append("🔥 Flame detected! IR sensor triggered")
-            except: pass
+            # try:
+            #     if "ir_detect" in latest and int(latest["ir_detect"]) == 0:
+            #         alert_messages.append("🔥 Flame detected! IR sensor triggered")
+            # except: pass
 
             # AQI alert
             for aqi_col in ["Air_Purity", "air_quality", "AQI"]:
@@ -1399,11 +1399,11 @@ while True:
                             style = "background-color: #6d4c41; color: white;"
                         elif orig_col in BINARY_COLS and int(float(val)) == 1:
                             style = "background-color: #c62828; color: white;"
-                        elif orig_col == "ir_detect":
-                            if int(float(val)) == 0:   # 0 = flame
-                                style = "background-color: #c62828; color: white;"
-                            else:
-                                style = ""  # 1 = safe
+                        # elif orig_col == "ir_detect":
+                        #     if int(float(val)) == 0:   # 0 = flame
+                        #         style = "background-color: #c62828; color: white;"
+                        #     else:
+                        #         style = ""  # 1 = safe
                         
                         elif orig_col in BINARY_COLS:
                             # keep other binary sensors normal (1 means detection)
